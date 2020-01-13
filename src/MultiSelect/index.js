8f7065e507fa184bdf8dfcf865185e53f9c63250
igorsheg-wix <igors@wix.com>
2020-01-13 14:09:22 +0200

    Multiselect comp wip

--- src/MultiSelect/index.js
+++ src/MultiSelect/index.js
@@ -1,50 +1,112 @@
 import register from "preact-custom-element";
-import {useState, useEffect} from "preact/hooks"
-import {h} from "preact"
+import { useState, useEffect, useRef } from "preact/hooks";
+import { h } from "preact";
+import "./styles.scss";
 
+const Dropdown = ({ items, clickHandler }) => {
+	useEffect(() => {}, [items]);
 
-const Dropdown = ({items, clickHandler}) => {
-   return (
-			<ul class="dropwdown">
-				{items.map(x => (
-					<li key={x.id}>
-						<label>
-							<input checked={x.checked} id={x.id} onChange={clickHandler} type="checkbox" />
-							{x.name}
-						</label>
-					</li>
-				))}
-			</ul>
-		);
+	return (
+		<ul class="dropwdown">
+			{items.map(x => (
+				<li className={x.checked ? "checked" : undefined} key={x.id}>
+					<label>
+						<input
+							checked={x.checked}
+							id={x.id}
+							onChange={clickHandler}
+							type="checkbox"
+						/>
+						{x.name}
+					</label>
+				</li>
+			))}
+		</ul>
+	);
 };
 
-const Multiselect = (props) => {
-    const [data, setData] = useState(null);
-    const [checked, setChecked] = useState([]);
+const Tags = ({ items, clickHandler }) => {
+	return (
+		<div className="tags">
+			{items.map(item => (
+				<span>
+					{item.name}{" "}
+					<i id={item.id} onClick={clickHandler}>
+						⨯
+					</i>
+				</span>
+			))}
+		</div>
+	);
+};
+
+const Multiselect = props => {
+	const [data, setData] = useState(null);
+	const [checked, setChecked] = useState([]);
+	const [unChecked, setUnChecked] = useState([]);
+	const [open, setOpen] = useState(false);
+	const [searchTerm, setSearchTerm] = useState('');
+	const [filteredData, setFilteredData] = useState([]);
 
 	useEffect(() => {
-		props.data && setData(JSON.parse(props.data));
-    }, [props]);
+		props && props.data && setData(JSON.parse(props.data));
+	}, [props]);
 
 	useEffect(() => {
-        console.log(checked);
-    }, [checked]);
+		setFilteredData(unChecked);
+	}, [unChecked]);
+
+	useEffect(() => {
+		if (data) {
+			setChecked(data.filter(item => item.checked));
+			setUnChecked(data.filter(item => !item.checked));
+		}
+	}, [data]);
 
 
-	useEffect(() => { 
-        data && setChecked(data.filter(item => item.checked))
-    }, [data]);
-    
-    const clickHandler = (e) => {
-        data.filter(item => item.id === e.target.id).map(x => x.checked = !x.checked)
-        setData([...data])
-    }
-		if (!data) return null
-		return (
-			<div>
-				<input type="text" />
-				<Dropdown clickHandler={clickHandler} items={data} />
+	const clickHandler = e => {
+		data
+			.filter(item => item.id === e.target.id)
+			.map(x => (x.checked = !x.checked));
+		setData([...data]);
+	};
+
+
+	useEffect(() => {
+		if (searchTerm.length) {
+			let filter = unChecked.filter(
+				item => item.name.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1
+			);
+			setFilteredData(filter);
+		}  else {
+			setFilteredData(unChecked);
+		}
+	}, [searchTerm]);
+
+
+	let timeout;
+	const termSearchHandler = (e) =>{
+		if (timeout) {
+			clearTimeout(timeout);
+		}
+		timeout = setTimeout(() => {
+			setSearchTerm(e.target.value)
+		}, 300)
+	}
+
+	if (!data) return null;
+	return (
+		<div>
+			<div className="fakeInput">
+				<Tags clickHandler={clickHandler} items={checked} />
+				<input
+					onInput={termSearchHandler}
+					onClick={() => setOpen(true)}
+					type="text"
+				/>
 			</div>
-		);
-}
-register(Multiselect, "x-multiselect", ['data']);
+			<Dropdown clickHandler={clickHandler} items={filteredData} />
+		</div>
+	);
+};
+register(Multiselect, "x-multiselect", ["data"]);
