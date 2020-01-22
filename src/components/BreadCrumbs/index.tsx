528b446a6801927cb86c2b4d8b4f1ea2731b6aed
igorsheg-wix <igors@wix.com>
2020-01-22 17:06:10 +0200

    Breadcrumbs wip

--- src/components/BreadCrumbs/index.tsx
+++ src/components/BreadCrumbs/index.tsx
@@ -14,16 +14,16 @@ interface Props {
     data: string
 }
 
-const Crumb = ({ item }) => (
+const Crumb = ({ item, clickHandler }) => (
   <StyledCrumb>
-    <p>{item.label}</p>
+    <button onClick={() => clickHandler(item)} type="button">{item.label}</button>
   </StyledCrumb>
 );
 
 
 const BreadCrumbs: FunctionComponent<Props> = ({ data }) => {
     const [dataState, setData] = useState<Crumb[] | null>(null);
-    const componentRef = useRef();
+    const componentRef = useRef<HTMLUListElement>();
 
     useEffect(() => {
     if (data) {
@@ -33,29 +33,73 @@ const BreadCrumbs: FunctionComponent<Props> = ({ data }) => {
     }, [data]);
 
 
-    const dispatchEvent = useCustomEvent({
-        ref: componentRef,
-        eventName: 'crumbClicked',
-        data: '',
-    })
+    const clickHandler = (item) => {
+        const dispatchEvent = useCustomEvent({
+            ref: componentRef,
+            eventName: 'crumbClicked',
+            data: item,
+        })
+
+        dispatchEvent();
+    }
 
     if (!dataState) return null;
 
     return (
-      <App ref={componentRef}>
-        <StyledCrumbs>
-          {dataState.map((crumb) => <Crumb key={crumb.label} item={crumb} />)}
+      <App>
+        <StyledCrumbs ref={componentRef}>
+          {dataState.map((crumb) => (
+            <Crumb
+              clickHandler={clickHandler}
+              key={crumb.label}
+              item={crumb}
+            />
+        ))}
         </StyledCrumbs>
       </App>
     );
 };
 
-const StyledCrumbs = styled('ul')`
-
+const StyledCrumbs: any = styled('ul')`
+list-style: none;
+display: flex;
+flex-direction: row;
+margin: 0;
+padding: 0;
 `;
 
-const StyledCrumb = styled('li')`
+const StyledCrumb = styled("li")`
+  display: flex;
+  align-items: center;
+  max-height: 30px;
 
+  button {
+    border: none;
+    background: transparent;
+    font-size: 14px;
+    color: ${(props) => props.theme.colors.$D10};
+    padding: 0;
+    display: flex;
+    border-radius: 30em;
+    padding: 6px 0;
+    &:hover {
+        cursor: pointer;
+      background: ${(props) => props.theme.colors.$B40};
+      box-shadow: 
+      -10px 0 0 0 ${(props) => props.theme.colors.$B40}, 
+      10px 0 0 0 ${(props) => props.theme.colors.$B40};
+    }
+  }
+
+  &:not(:last-child):after {
+    content: "";
+    background: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' width='24' height='24'%3E%3Cpath fill='none' d='M0 0h24v24H0z'/%3E%3Cpath d='M13.172 12l-4.95-4.95 1.414-1.414L16 12l-6.364 6.364-1.414-1.414z' fill='rgba(182,193,205,1)'/%3E%3C/svg%3E");
+    background-size: 20px 20px;
+    display: inline-block;
+    margin: 0 18px;
+    height: 16px;
+    width: 16px;
+  }
 `;
 
 
