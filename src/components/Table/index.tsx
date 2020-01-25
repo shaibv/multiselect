512727245ced51951a8cd86b5bbd780cdfd84483
igorsheg-wix <igors@wix.com>
2020-01-25 02:33:38 +0200

    Table styling wip

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -96,7 +96,8 @@ const Styles = styled("div")`
   .table {
     font-size: 14px;
     background: ${(props) => props.theme.colors.$D80};
-    border-radius: 8px;
+    border-bottom-left-radius: 8px;
+    border-bottom-right-radius: 8px;
     box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.1);
     width: 100%;
     overflow: hidden;
