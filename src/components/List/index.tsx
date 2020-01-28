bc8ad6639121371881e9271bd154a180eb8cc40a
igorsheg-wix <igors@wix.com>
2020-01-29 01:06:47 +0200

    List wip

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -13,6 +13,8 @@ type Item = {
 };
 
 
+const isHtmlElement = (string) => /<(br|basefont|hr|input|source|frame|param|area|meta|!--|col|link|option|base|img|wbr|!DOCTYPE).*?>|<(a|abbr|acronym|address|applet|article|aside|audio|b|bdi|bdo|big|blockquote|body|button|canvas|caption|center|cite|code|colgroup|command|datalist|dd|del|details|dfn|dialog|dir|div|dl|dt|em|embed|fieldset|figcaption|figure|font|footer|form|frameset|head|header|hgroup|h1|h2|h3|h4|h5|h6|html|i|iframe|ins|kbd|keygen|label|legend|li|map|mark|menu|meter|nav|noframes|noscript|object|ol|optgroup|output|p|pre|progress|q|rp|rt|ruby|s|samp|script|section|select|small|span|strike|strong|style|sub|summary|sup|table|tbody|td|textarea|tfoot|th|thead|time|title|tr|track|tt|u|ul|var|video).*?<\/\2>/i.test(string)
+
 const SkeletonData = () => (
   <StyledList>
     <Keys>
@@ -40,7 +42,9 @@ const ListedData = ({ data }) => (
     <Values>
       {data.map((item) => (
         // @ts-ignore
-        <li innerHTML={item.value} key={item.value} />
+        isHtmlElement(item.value) ? <li innerHTML={item.value} key={item.value} />
+        : <li key={item.value}>{item.value}</li>
+
     ))}
     </Values>
   </StyledList>
