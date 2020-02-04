b6f4594b91b4cc2d9179869243187db9e28b6e5d
igorsheg-wix <igors@wix.com>
2020-02-04 02:07:55 +0200

    Storybook wip

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,57 +1,108 @@
-import { addParameters, addDecorator } from "@storybook/preact";
-import { DocsPage, DocsContainer } from "@storybook/addon-docs/blocks";
-import { withInfo } from "@storybook/addon-info";
+import {  addDecorator } from "@storybook/preact";
 import {h} from "preact";
-import {useEffect} from "preact/compat"
+import {useEffect, useState} from "preact/compat"
 import styled from 'styled-components';
+import { useTable } from "react-table";
 
 
-const TableComponent = ({ propDefinitions }) => {
-  const props = propDefinitions.map(({ type, name, required, description, defaultValue }) => {
-      return (
-        <tr key={name}>
-          <td>
-            {`${name}, ${required ? '*' : '' }`}
-          </td>
-          <td>{type.name}</td>
-          <td>{defaultValue ? defaultValue : "-"}</td>
-          <td>{description}</td>
-        </tr>
-      );
+
+const Table = ({ data }) => {
+
+  const columns = [
+    {
+      Header: "Name",
+      accessor: "name"
+    },
+    {
+      Header: "Type",
+      accessor: "type"
+    },
+    {
+      Header: "Default Value",
+      accessor: "defaultValue"
+    },
+    {
+      Header: "Required",
+      accessor: "required"
     }
-  );
+  ];
 
+  // Use the state and functions returned from useTable to build your UI
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow
+  } = useTable({
+    columns,
+    data
+  });
+
+  // Render the UI for your table
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>name</th>
-          <th>type</th>
-          <th>default</th>
-          <th>description</th>
-        </tr>
-      </thead>
-      <tbody>{props}</tbody>
-    </table>
+    <StyledTable>
+      <h3>Component Props</h3>
+      <div  {...getTableProps()} className="table">
+        <div>
+          {headerGroups.map(headerGroup => (
+            <div
+              key={headerGroup.id}
+              {...headerGroup.getHeaderGroupProps()}
+              className="tr header"
+            >
+              {headerGroup.headers.map(column => (
+                <div
+                  key={column.id}
+                  {...column.getHeaderProps()}
+                  className="th"
+                >
+                  {column.render("Header")}
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
+
+        <div {...getTableBodyProps()}>
+          {rows.map(row => {
+            prepareRow(row);
+            return (
+              <div key={row.id} {...row.getRowProps()} className="tr">
+                {row.cells.map(cell => (
+                  <div key={cell.value} {...cell.getCellProps()} className="td">
+                    {cell.render("Cell")}
+                  </div>
+                ))}
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </StyledTable>
   );
-};
+}
 
 const StoryWrap = ({ story }) => {
   const name = story.type.__docgenInfo ? story.type.__docgenInfo.displayName : story.type.name;
   const props = story.type.__docgenInfo && story.type.__docgenInfo.props;
-
-  useEffect(() => {
-    const keys = Object.values(props);
-    console.log(keys);
+  const [parsedProps, setProps] = useState(null)
   
-  }, [props]);
+
+  useEffect(async () => {
+    console.log(props);
+    const propsArr = Object.values(props);
+    const mappedProps = propsArr.map(prop => ({...prop, type: prop.type.name}))
+    setProps(mappedProps);
+    console.log(mappedProps);
+  }, [story]);
 
   return (
     <StyledStory>
       <div className="content">
         <h1>{name}</h1>
         <div className="story">{story}</div>
-        {props && <TableComponent propDefinitions={Object.values(props)} />}
+        {parsedProps && <Table data={Object.values(parsedProps)} />}
       </div>
     </StyledStory>
   );
@@ -90,7 +141,51 @@ const StyledStory = styled.div`
 
 
 
+const StyledTable = styled.div`
+margin: 30px 0 0 0;
+  .table {
+    font-size: 14px;
+    width: 100%;
+    overflow: hidden;
 
+    .tr.header {
+      height: 42px;
+      font-size: 10px;
+      color: #999;
+      font-weight: 600;
+      text-transform: uppercase;
+      &:hover {
+        cursor: default;
+      }
+    }
+
+    .tr {
+      height: 42px;
+      width: 100%;
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+
+      &:hover {
+        transition: all 120ms ease;
+      }
+
+      .th,
+      .td {
+        display: block;
+        flex: 1;
+        text-align: left;
+        overflow-x: hidden;
+        text-overflow: ellipsis;
+        align-items: center;
+      }
+
+      .th:first-child,
+      .td:first-child {
+      }
+    }
+  }
+`;
   
 addDecorator(storyFn => (
   <StoryWrap story={storyFn()} />
